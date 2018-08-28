9e2bd24dd4c1b54b88e66bfd6292833131653218
enrico.detoma <enrico.detoma@gmail.com>
2018-08-28 09:41:50 +0200

    -

--- twsapi/Shared/EClientSocket.h
+++ twsapi/Shared/EClientSocket.h
@@ -25,8 +25,7 @@ public:
 	explicit EClientSocket(EWrapper *ptr, EReaderSignal *pSignal = 0);
 	virtual ~EClientSocket();
 
-	// ED virtual
-	virtual bool eConnect( const char *host, int port, int clientId = 0, bool extraAuth = false);
+	bool eConnect( const char *host, int port, int clientId = 0, bool extraAuth = false);
 	// override virtual funcs from EClient
 	void eDisconnect(bool resetState = true);
 
